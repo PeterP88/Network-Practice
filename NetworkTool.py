7f52ed50a7f28ca8e56035e27c02470fab8a4f2f
fish <poznik.p@gamil.com>
2023-11-27 19:17:56 +1100

    Orginal Commit And Test Push

--- NetworkTool.py
+++ NetworkTool.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import subprocess
+import ipaddress
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-sdfgsdfgs
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #saving network cidr
+    network_cidr = "192.168.1.0/24"
+
+    #creates a network object from the network cidr
+    network = ipaddress.ip_network(network_cidr)
+
+
+
+
